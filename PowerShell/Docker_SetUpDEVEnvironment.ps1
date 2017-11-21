306ae3466bbab2e1405a85c3fed85f2f02cbddf2
waldo1001 <github@waldo.be>
2017-11-21 23:24:56 +0100

    Add PowerShell scripts

---
+++ PowerShell/Docker_SetUpDEVEnvironment.ps1
@@ -0,0 +1,30 @@
+﻿. '.\PowerShell\Settings.ps1'
+
+New-NavContainer `
+    -accept_eula `
+    -containerName $DEVServer `
+    -imageName $DockerImage `
+    -licenseFile $LicenseFile `
+    -memoryLimit $MemoryDEVServer `
+    -updateHosts `
+    -auth NavUserPassword `
+    -includeCSide `
+    -Verbose `
+    -Credential $Credential `
+    -doNotExportObjectsToText
+
+New-NavContainer `
+    -accept_eula `
+    -containerName $TESTServer `
+    -imageName $DockerImage `
+    -licenseFile $LicenseFile `
+    -memoryLimit $MemoryTESTServer `
+    -updateHosts `
+    -auth NavUserPassword `
+    -includeCSide `
+    -Verbose `
+    -Credential $Credential `
+    -doNotExportObjectsToText 
+
+Start-Process "http://$($DEVServer):8080/"
+
